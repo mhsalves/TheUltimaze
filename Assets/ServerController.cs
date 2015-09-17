51db3ef0d682ab60acfe51c476627cb542e66ffe
Eduardo Façanha <eduardo_facanha_f@hotmail.com>
2015-09-17 12:32:58 -0400

    iniciando

---
+++ Assets/ServerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerController : MonoBehaviour {
+	public string ipToConnect;
+	public int portToConnect;
+	public int numberPlayers;
+	public GameObject objectControl;
+	public player playeR;
+	
+	// Use this for initialization
+	void Start () {
+
+		ipToConnect = Network.player.ipAddress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void StartServer(){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+			Debug.Log(Network.InitializeServer(numberPlayers, portToConnect, false));
+			OnConnectedToServer();
+		}
+	}
+	
+	void ConnectToServer(){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+			Debug.Log(Network.Connect(ipToConnect, portToConnect));
+		}
+	}
+	
+	void OnGUI(){
+		if(Network.peerType == NetworkPeerType.Disconnected){
+			if(GUI.Button(new Rect(Screen.width/2-100, 10, 200, 30), "Inicializar Servidor")){
+				StartServer();
+			}
+			ipToConnect = GUI.TextField(new Rect(Screen.width/2-100, 40, 200, 30), ipToConnect);
+			if(GUI.Button(new Rect(Screen.width/2-100, 70, 200, 30), "Conectar ao Servidor")){
+				ConnectToServer();
+				
+			}
+			
+		}
+		
+		
+	}
+	
+	void OnConnectedToServer(){
+		GameObject player = Network.Instantiate(objectControl,new Vector3 (23.5f, 1.8f, 12.8f), transform.rotation, 0) as GameObject;
+		playeR.payer = player;
+		//player.GetComponent<PlayerBehaviour>().GetCamera();
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = playeR.payer.GetComponent<Rigidbody>().transform.position;
+			stream.Serialize(ref syncPosition);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+			playeR.payer.GetComponent<Rigidbody>().transform.position = syncPosition;
+		}
+	}
+	
+	
+	
+	
+}
---
+++ Assets/player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	//public GameObject ob;
+	public GameObject payer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Input.GetKey(KeyCode.Space)){
+//			GetComponent<Rigidbody>().velocity = new Vector3 (0,0, 0);
+//
+//
+//		}
+
+		Movimentacao ();
+	}
+
+
+
+	void Movimentacao() {
+		if ((Input.touchCount > 0 && Input.touchCount < 2) || (Input.GetKey(KeyCode.RightArrow))) {
+			//ob.transform.Rotate (10f, 0f, 0f);
+			//transform.eulerAngles = new Vector2 (0, 90);
+			//print(ob.transform.eulerAngles);
+			//ob.GetComponent<Animation>().Play("Walk");
+			payer.transform.Translate (-Vector3.left * 0.025f );
+
+		} 
+		
+		else if ((Input.touchCount > 1 && Input.touchCount < 3) || (Input.GetKey(KeyCode.LeftArrow))) {
+			//transform.eulerAngles = new Vector2 (0, -90);
+			//print(ob.transform.eulerAngles);
+			//ob.GetComponent<Animation>().Play("Walk");
+			payer.transform.Translate (Vector3.left * 0.025f );
+		
+		} 
+	
+		else if ((Input.touchCount > 2 && Input.touchCount < 4) || (Input.GetKey(KeyCode.UpArrow))) {
+			transform.eulerAngles = new Vector2 (0, 0);
+			//print(ob.transform.eulerAngles);
+			//ob.GetComponent<Animation>().Play("Walk");
+			payer.transform.Translate (Vector3.forward * 0.025f );
+
+		} 
+		else if ((Input.touchCount > 3 && Input.touchCount < 5) || (Input.GetKey(KeyCode.DownArrow))) {
+			//transform.eulerAngles = new Vector2 (0, 180);
+			//print(ob.transform.eulerAngles);
+			//ob.GetComponent<Animation>().Play("Walk");
+			payer.transform.Translate (-Vector3.forward * 0.025f);
+
+		} 
+		//else ob.GetComponent<Animation>().Play("Idle");
+	}
+
+}
